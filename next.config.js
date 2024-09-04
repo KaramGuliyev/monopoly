e89eb77737eaa4c2c94dc0433dc729a07820cc95
Karam Guliyev <karamguliyev10@gmail.com>
2024-09-04 16:47:05 +0400

    Backend Seperted

--- next.config.js
+++ next.config.js
@@ -7,4 +7,15 @@ module.exports = {
       },
     ]
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 }
